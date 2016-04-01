90e4f6f8d1932e0f02e41384aaa1d45624cd0b99
crystalDf <chendong333@gmail.com>
2016-04-01 23:14:53 +0800

    Chapter 07 Regular Expressions

--- vowelRegex.py
+++ vowelRegex.py
@@ -8,3 +8,25 @@ print(vowel_regex.findall('RoboCop eats baby food. BABY FOOD.'))
 
 consonant_regex = re.compile(r'[^aeiouAEIOU]')
 print(consonant_regex.findall('Robo op eats baby food. BABY FOOD.'))
+
+"""
+You can also use the caret symbol (^) at the start of a regex to
+indicate that a match must occur at the beginning of the searched text.
+Likewise, you can put a dollar sign ($) at the end of the regex to
+indicate the string must end with this regex pattern.
+And you can use the ^ and $ together to
+indicate that the entire string must match the regex.
+"""
+begins_with_hello = re.compile(r'^Hello')
+match_object = begins_with_hello.search('Hello world!')
+print(match_object.group())
+match_object = begins_with_hello.search('He said hello.')
+print(match_object)
+
+whole_string_is_num = re.compile(r'^\d+$')
+match_object = whole_string_is_num.search('1234567890')
+print(match_object.group())
+match_object = whole_string_is_num.search('12345xyz67890')
+print(match_object)
+match_object = whole_string_is_num.search('12  34567890')
+print(match_object)
