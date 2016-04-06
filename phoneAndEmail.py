94f5d6bc0fa9462b1987b39a61627ebed7d28ac8
crystalDf <chendong333@gmail.com>
2016-04-06 23:21:22 +0800

    Chapter 07 Regular Expressions

---
+++ phoneAndEmail.py
@@ -0,0 +1,13 @@
+#! python3
+
+import pyperclip
+import re
+
+phone_regex = re.compile(r'''(
+(\d{3}|\(\d{3}\))?
+(\s|-|\.)?
+(\d{3})
+(\s|-|\.)
+(\d{4})
+(\s*(ext|x|ext.)\s*(\d{2,5}))?
+)''', re.VERBOSE)
