a69f7eb24171bcb707f616c6a9720eff7a14ca9d
crystalDf <chendong333@gmail.com>
2016-04-09 23:45:49 +0800

    Chapter 07 Regular Expressions

--- myStrip.py
+++ myStrip.py
@@ -5,12 +5,12 @@ def my_strip(str_to_strip, str_to_remove=''):
     remove_regex = re.compile(str_to_remove)
     removed_string = remove_regex.sub(r'', str_to_strip)
 
-    print(removed_string)
-
-    # strip_regex = re.compile(r'^\s*(\w*)\s*$')
-    strip_regex = re.compile(r'([^\s]*)\s*$')
+    strip_regex = re.compile(r'^\s*(\w.*\w)\s*$')
     match_object = strip_regex.search(removed_string)
     print(match_object.group(1))
 
 
 my_strip('   haha   test  haha   ', 'test')
+my_strip('   haha   test  haha   ')
+my_strip('haha   test  haha   ')
+my_strip('haha   test  haha')
